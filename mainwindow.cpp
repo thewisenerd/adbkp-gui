4c4facf2a66f47d82b613267f84d010fed810834
Vineeth Raj <vineethraj49@gmail.com>
2013-09-03 07:32:32 +0530

    recalibrate

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <limits>
 
 using namespace std;
 
@@ -16,8 +17,8 @@ bool multi_touch = false;
 QProcess *exec = new QProcess();
 
 // Default CM9 config
-QString himax="event3";
-QString keypad="event4";
+QString himax="event1";
+QString keypad="event2";
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -93,8 +94,8 @@ void MainWindow::rom_select()
 {
     QString rom_name=this->ui->rom_select->currentText();
     if (rom_name=="CM9") {
-        himax="event3";
-        keypad="event4";
+        himax="event1";
+        keypad="event2";
     }
     else if (rom_name=="MiniCM9"){
         himax="event4";
@@ -111,28 +112,50 @@ string inttoa(int a) {
 
 void MainWindow::recalibrate()
 {
-    system("timeout 0.5s ./bin/adb shell getevent > ./temp1.txt");
-    system("grep -n himax-touchscreen ./temp1.txt | cut -c 1-2 > ./temp2.txt");
-    string s;
-    ifstream temp3("./temp2.txt");
-    getline(temp3,s);
-    temp3.close();
+    system("timeout 2s ./bin/adb shell getevent > ./keycodes.txt");
+    system("grep -n himax-touchscreen ./keycodes.txt | cut -c 1-2 > ./himax_event_line.txt");
+    system("grep -n pico-keypad ./keycodes.txt | cut -c 1-2 > ./keypad_event_line.txt");
 
-    int line1 = atoi(s.c_str());
-    int line0 = line1 - 1;
-    std::string temp4=inttoa(line0);
-    system(("sed '"+temp4+"q;d' ./temp1.txt | cut -c 15- > ./temp5.txt").c_str());
+    string himax_line_plus_one;
+    ifstream himax_event_line_get("./himax_event_line.txt");
+    getline(himax_event_line_get,himax_line_plus_one);
+    himax_event_line_get.close();
 
-    string himax_val;
-    ifstream temp6("./temp5.txt");
-    getline(temp6,himax_val);
-    temp6.close();
+    string keypad_line_plus_one;
+    ifstream keypad_event_line_get("./keypad_event_line.txt");
+    getline(keypad_event_line_get,keypad_line_plus_one);
+    keypad_event_line_get.close();
 
-    system("rm ./temp1.txt");
-    system("rm ./temp2.txt");
-    system("rm ./temp5.txt");
+    int line0 = atoi(himax_line_plus_one.c_str());
+    int line1 = line0 - 1;
 
-    himax = QString::fromUtf8(himax_val.c_str());
+    int line3 = atoi(keypad_line_plus_one.c_str());
+    int line2 = line3 - 1;
+
+    std::string himax_event_line=inttoa(line1);
+    std::string keypad_event_line=inttoa(line2);
+
+    system(("sed '"+himax_event_line+"q;d' ./keycodes.txt | cut -c 26- > ./himax_event_code.txt").c_str());
+    system(("sed '"+keypad_event_line+"q;d' ./keycodes.txt | cut -c 26- > ./keypad_event_code.txt").c_str());
+
+    char himax_val[6];
+    ifstream get_himax_code("./himax_event_code.txt");
+    get_himax_code.get(himax_val, 7);
+    get_himax_code.close();
+
+    char keypad_val[6];
+    ifstream get_keypad_code("./keypad_event_code.txt");
+    get_keypad_code.get(keypad_val, 7);
+    get_keypad_code.close();
+
+    system("rm ./keycodes.txt");
+    system("rm ./himax_event_line.txt");
+    system("rm ./himax_event_code.txt");
+    system("rm ./keypad_event_line.txt");
+    system("rm ./keypad_event_code.txt");
+
+    himax =  QString::fromUtf8(himax_val);
+    keypad =  QString::fromUtf8(keypad_val);
 }
 
 void MainWindow::touch_type_set()
@@ -163,7 +186,7 @@ void myQWidget::mousePressEvent(QMouseEvent *event)
             exec->execute("./bin/adb shell \"sendevent /dev/input/"+himax+" 3 48 1 && sendevent /dev/input/"+himax+" 3 53 "+QString::number(static_cast<int>(floor((((static_cast<double>(event->pos().x()))/320)*1024) + 0.5f)))+" && sendevent /dev/input/"+himax+" 3 54 "+QString::number(static_cast<int>(floor((((static_cast<double>(event->pos().y()))/480)*910) + 0.5f)))+" && sendevent /dev/input/"+himax+" 0 0 0\"");
                 }
     else {
-        exec->execute("./bin/adb shell \"sendevent /dev/input/"+himax+" 3 57 0 && sendevent /dev/input/"+himax+" 3 48 1 && sendevent /dev/input/"+himax+" 3 53 "+QString::number(static_cast<int>(floor((((static_cast<double>(event->pos().x()))/320)*1024) + 0.5f)))+" && sendevent /dev/input/"+himax+" 3 54 "+QString::number(static_cast<int>(floor((((static_cast<double>(event->pos().y()))/480)*910) + 0.5f)))+" && sendevent /dev/input/"+himax+" 0 2 0 && sendevent /dev/input/"+himax+" 3 57 1 && sendevent /dev/input/"+himax+" 3 48 1 && sendevent /dev/input/"+himax+" 3 53 "+QString::number(static_cast<int>(floor((((static_cast<double>(320-event->pos().x()))/320)*1024) + 0.5f)))+" && sendevent /dev/input/"+himax+" 3 54 "+QString::number(static_cast<int>(floor((((static_cast<double>(480-event->pos().y()))/480)*910) + 0.5f)))+" && sendevent /dev/input/"+himax+" 0 2 0 && sendevent /dev/input/"+himax+" 0 0 0\"");
+        exec->execute("echo \"sendevent /dev/input/"+himax+" 3 57 0 && sendevent /dev/input/"+himax+" 3 48 1 && sendevent /dev/input/"+himax+" 3 53 "+QString::number(static_cast<int>(floor((((static_cast<double>(event->pos().x()))/320)*1024) + 0.5f)))+" && sendevent /dev/input/"+himax+" 3 54 "+QString::number(static_cast<int>(floor((((static_cast<double>(event->pos().y()))/480)*910) + 0.5f)))+" && sendevent /dev/input/"+himax+" 0 2 0 && sendevent /dev/input/"+himax+" 3 57 1 && sendevent /dev/input/"+himax+" 3 48 1 && sendevent /dev/input/"+himax+" 3 53 "+QString::number(static_cast<int>(floor((((static_cast<double>(320-event->pos().x()))/320)*1024) + 0.5f)))+" && sendevent /dev/input/"+himax+" 3 54 "+QString::number(static_cast<int>(floor((((static_cast<double>(480-event->pos().y()))/480)*910) + 0.5f)))+" && sendevent /dev/input/"+himax+" 0 2 0 && sendevent /dev/input/"+himax+" 0 0 0\"");
     }
 }
 
@@ -192,32 +215,32 @@ void myQWidget::paintEvent(QPaintEvent *)
 
 void MainWindow::pwr_btn_click_start()
 {
-    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 116 1\"");
+    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 116 1 && sendevent /dev/input/"+keypad+" 0 0 0\"");
 }
 
 void MainWindow::pwr_btn_click_end()
 {
-    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 116 0\"");
+    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 116 0 && sendevent /dev/input/"+keypad+" 0 0 0\"");
 }
 
 void MainWindow::vol_up_btn_click_start()
 {
-    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 115 1\"");
+    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 115 1 && sendevent /dev/input/"+keypad+" 0 0 0\"");
 }
 
 void MainWindow::vol_up_btn_click_end()
 {
-    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 115 0\"");
+    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 115 0 && sendevent /dev/input/"+keypad+" 0 0 0\"");
 }
 
 void MainWindow::vol_dn_btn_click_start()
 {
-    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 114 1\"");
+    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 114 1 && sendevent /dev/input/"+keypad+" 0 0 0\"");
 }
 
 void MainWindow::vol_dn_btn_click_end()
 {
-    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 114 0\"");
+    exec->execute("./bin/adb shell \"sendevent /dev/input/"+keypad+" 1 114 0 && sendevent /dev/input/"+keypad+" 0 0 0\"");
 }
 
 void MainWindow::home_btn_click_start()
